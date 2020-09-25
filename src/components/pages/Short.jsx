a716e81ea87f2003da1b0f8eed73d51ad21b0649
willwearing <willwearing@gmail.com>
2020-09-25 16:16:05 -0600

    added axios post and set data to state

--- src/components/pages/Short.jsx
+++ src/components/pages/Short.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Short = () => {
   const { handleSubmit, register, errors } = useForm();
-  const onSubmit = (input) => console.log(input);
+  const [shortURL, setShortUrl] = useState([]);
+  const POST_URL = "https://rel.ink/api/links/";
+  const onSubmit = (input) => {
+    axios
+      .post(POST_URL, input)
+      .then((res) => {
+        setShortUrl(res.data);
+      })
+      .catch((err) => console.log(`Error: ${err}`))
+      .finally();
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          name="urlShort"
-          placeholder="Enter your URL"
+          name="url"
+          placeholder="Enter your URL - Copy and Paste from your browser - This will break if not"
           ref={register({
             required: "Required",
             patter: {
@@ -20,7 +31,7 @@ const Short = () => {
             },
           })}
         />
-        {errors.urlShort && errors.urlShort.message}
+        {errors.url && errors.url.message}
         <button type="submit">Shorten It!</button>
       </form>
     </div>
